31fc3aa41aabd27a7a95f4abbbf90be89f90b4a8
httpfang <114400511+httpfang@users.noreply.github.com>
2023-08-14 05:53:11 +0000

    completed the html part of the game

---
+++ script.js
@@ -0,0 +1,19 @@
+const wheatherScorer = () => {
+	let wheather = ['rainy', 'sunny', 'overcast']
+	let result = math.floor(math.random(wheather.length*3))
+	if(result == rainy) {
+		console.log(1)
+	}
+		if(result == sunny) {
+		console.log(-1)
+	}
+		if(result == overcast) {
+		console.log(0)
+	}
+	else{
+		console.log('winter')
+	}
+	
+}
+
+console.log(wheatherScorer(result))
